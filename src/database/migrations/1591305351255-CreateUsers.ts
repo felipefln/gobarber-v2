8f8a5ea17ed072f3788bb168bbf541e7d795dbf7
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2020-06-11 10:52:38 -0300

    Feat: Criado domain de usuário.

--- src/database/migrations/1591305351255-CreateUsers.ts
+++ src/database/migrations/1591305351255-CreateUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateAppointments1591285019088 implements MigrationInterface {
+export default class CreateUsers1591305351255 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "appointments",
+        name: "users",
         columns: [
           {
             name: "id",
@@ -14,14 +14,29 @@ export class CreateAppointments1591285019088 implements MigrationInterface {
             default: "uuid_generate_v4()",
           },
           {
-            name: "provider",
+            name: "name",
             type: "varchar",
             isNullable: false,
           },
           {
-            name: "date",
-            type: "timestamp with time zone",
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
             isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "update_at",
+            type: "timestamp",
+            default: "now()",
           },
         ],
       }),
@@ -29,6 +44,6 @@ export class CreateAppointments1591285019088 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("appointments");
+    await queryRunner.dropTable("users");
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 
-import express from "express";
+import express, { json } from "express";
 import routes from "./routes";
 
 import "./database";
 
 const app = express();
 
+app.use(json());
 app.use(routes);
 app.listen(3333, () => {
   console.log("Segue filhoo port 3333");
