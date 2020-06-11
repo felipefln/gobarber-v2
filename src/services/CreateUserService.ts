39946bfd69de904976c06e3bf96b4d6859642760
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2020-06-11 12:07:24 -0300

    Feat: Criado função para criptografar a senha do usuário utilizando BcryptJS.

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from "typeorm";
+import { hash } from "bcryptjs";
 import User from "../models/User";
 
 interface Request {
@@ -19,10 +20,12 @@ class CreateUserService {
       throw new Error("Email já cadastrado/Usuário existente.");
     }
 
+    const hashedPasword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPasword,
     });
 
     await usersRepository.save(user);
