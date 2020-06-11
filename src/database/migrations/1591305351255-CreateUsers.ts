e14300720592291d899571d6ecf3299b0d71b1c1
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2020-06-11 11:55:15 -0300

    Feat: Criado relacionamento agendamento/prestador e usuário.

--- src/database/migrations/1591305351255-CreateUsers.ts
+++ src/database/migrations/1591305351255-CreateUsers.ts
@@ -34,7 +34,7 @@ export default class CreateUsers1591305351255 implements MigrationInterface {
             default: "now()",
           },
           {
-            name: "update_at",
+            name: "updated_at",
             type: "timestamp",
             default: "now()",
           },
